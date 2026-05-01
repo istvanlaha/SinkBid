6ebc3b34b937243e5e67f3d3dc1bf60ff16ca2d8
István <istvanlaha@gmail.com>
2026-05-01 22:17:56 +0200

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import styles from './Footer.module.css'
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.inner}>
+        <div className={styles.logo}>SinkBid</div>
+        <div className={styles.links}>
+          <Link to="/" className={styles.link}>Aktív licitek</Link>
+          <Link to="/sold" className={styles.link}>Eladott tárgyak</Link>
+          <Link to="/how" className={styles.link}>Hogyan működik</Link>
+        </div>
+        <div className={styles.copy}>© 2025 SinkBid. Minden jog fenntartva.</div>
+      </div>
+    </footer>
+  )
+}
