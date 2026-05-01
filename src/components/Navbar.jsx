de631bc3b6eefc0b2f5087f5a8db6882646ad853
István <istvanlaha@gmail.com>
2026-05-01 22:13:29 +0200

    Create Navbar.jsx

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { useTranslation } from 'react-i18next'
+import { useBidStore } from '../store/bidStore'
+import i18n from '../i18n/i18n'
+import styles from './Navbar.module.css'
+
+const LANGS = [
+  { code: 'hu', label: 'HU', dir: 'ltr' },
+  { code: 'en', label: 'EN', dir: 'ltr' },
+  { code: 'ru', label: 'RU', dir: 'ltr' },
+  { code: 'ar', label: 'AR', dir: 'rtl' },
+]
+
+export default function Navbar() {
+  const { t } = useTranslation()
+  const { lang, setLang } = useBidStore()
+
+  const handleLang = (e) => {
+    const selected = e.target.value
+    setLang(selected)
+    i18n.changeLanguage(selected)
+    document.documentElement.dir = LANGS.find(l => l.code === selected)?.dir || 'ltr'
+    document.documentElement.lang = selected
+  }
+
+  return (
+    <nav className={styles.nav}>
+      <Link to="/" className={styles.logo}>
+        Sink<span>Bid</span>
+      </Link>
+      <div className={styles.right}>
+        <Link to="/" className={styles.link}>{t('nav.activeBids')}</Link>
+        <Link to="/sold" className={styles.link}>{t('nav.sold')}</Link>
+        <Link to="/how" className={styles.link}>{t('nav.howItWorks')}</Link>
+        <div className={styles.authLinks}>
+          <Link to="/login" className={styles.linkBtn}>{t('nav.login')}</Link>
+          <Link to="/register" className={styles.linkBtnPrimary}>{t('nav.register')}</Link>
+        </div>
+        <select className={styles.langSelect} value={lang} onChange={handleLang}>
+          {LANGS.map(l => (
+            <option key={l.code} value={l.code}>{l.label}</option>
+          ))}
+        </select>
+      </div>
+    </nav>
+  )
+}
