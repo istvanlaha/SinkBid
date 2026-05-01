767bc115c208d49ba4051c88a30583c0baaabff3
István <istvanlaha@gmail.com>
2026-05-01 21:58:07 +0200

    Create App.jsx

---
+++ src/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import Home from './pages/Home'
+import Sold from './pages/Sold'
+import HowItWorks from './pages/HowItWorks'
+
+export default function App() {
+  return (
+    <div style={{ minHeight: '100vh', display: 'flex', flexDirection: 'column' }}>
+      <Navbar />
+      <div style={{ flex: 1 }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sold" element={<Sold />} />
+          <Route path="/how" element={<HowItWorks />} />
+        </Routes>
+      </div>
+      <Footer />
+    </div>
+  )
+}
